825dce0edabe8151fe5b341b382ecb74c3d3e306
brunopaiva1 <brunopaivamaster10@gmail.com>
2024-03-17 20:03:26 -0300

    create plano

--- main.js
+++ main.js
@@ -1,14 +1,24 @@
 import * as THREE from 'three';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+// inicialize a cena
+var scene = new THREE.Scene();
+
+// criando uma camera
+var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.set(0, 3, 0);
 
 // Criando uma rederização
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
+var geometry = new THREE.PlaneGeometry(200, 200);
+var material = new THREE.MeshStandardMaterial({ color: 0x000000, side:THREE.DoubleSide });
+var surface = new THREE.Mesh(geometry, material);
+surface.receiveShadow = true;
+scene.add(surface);
+
 function animate(){
-    
+
 }
